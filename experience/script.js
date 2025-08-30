ed1ea0a2bd353127e83b388d2a58849d9eb12e29
Mohd <sami.mohd29@gmail.com>
2025-08-30 12:55:57 -0700

    set names to uppercase

--- experience/script.js
+++ experience/script.js
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Mitesh Pradhan";
+        document.title = "Experience | Portfolio Md Samiullah";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
